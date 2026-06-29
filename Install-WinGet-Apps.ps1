34817e422dffb802e4c3a885d6e501f22dd0a588
matter172 <matter172@service.zaman.my>
2026-06-30 02:47:31 +0800

    Create Install-WinGet-Apps.ps1

---
+++ Install-WinGet-Apps.ps1
@@ -0,0 +1,5 @@
+winget install -e --id Valve.Steam;
+winget install -e --id EpicGames.EpicGamesLauncher;
+winget install -e --id Discord.Discord;
+winget install -e --id Brave.Brave;
+winget install TechPowerUp.NVCleanstall;
